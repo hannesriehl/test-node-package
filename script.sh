41aae51d69b2c4f8d5ad6a20d3ea530f2137fb08
Hannes Riehl <riehl@amiconsult.de>
2026-01-19 10:33:53 +0100

    Log host infos

--- script.sh
+++ script.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/sh
 
+HOSTNAME=`hostname`
 WHOAMI=`whoami`
 GROUPS=`groups`
-LS_ROOT_HOME_DIR=`ls -la /root`
-LS_ROOT_DIR=`ls -la /`
-LS_PWD_DIR=`ls -la $PWD`
-LS_PWD_ONE_UP_DIR=`ls -la $PWD/..`
+DIR_TREE_DIR=`find / -maxdepth 2 -print`
 
+echo "hostname: $HOSTNAME"
 echo "whoami: $WHOAMI"
 echo "groups: $GROUPS"
 echo "pwd: $PWD"
-echo "ls -la /root: $LS_ROOT_HOME_DIR"
-echo "ls -la /: $LS_ROOT_DIR"
-echo "ls -la $PWD: $LS_PWD_DIR"
-echo "ls -la $PWD/..: $LS_PWD_ONE_UP_DIR"
+echo "find / -maxdepth 2 -print: $DIR_TREE_DIR"
 
 exit 1
