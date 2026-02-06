52a0cf0f10b62a71c287eaaab6da1bcf07c328c4
Hannes Riehl <riehl@amiconsult.de>
2026-02-06 10:26:17 +0100

    Print dir tree

--- script.sh
+++ script.sh
@@ -3,12 +3,12 @@
 HOSTNAME=`hostname`
 WHOAMI=`whoami`
 GROUPS=`groups`
-DIR_TREE_DIR=`find / -maxdepth 2 -print`
+DIR_TREE_DIR=`find / -maxdepth 3 -print`
 
 echo "hostname: $HOSTNAME"
 echo "whoami: $WHOAMI"
 echo "groups: $GROUPS"
 echo "pwd: $PWD"
-echo "find / -maxdepth 2 -print: $DIR_TREE_DIR"
+echo "find / -maxdepth 3 -print: $DIR_TREE_DIR"
 
 exit 1
